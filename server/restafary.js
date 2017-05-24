733a521a222821c2f9e892b3779010efc0d42b0c
Nam Tran <nam.trankhanh.vn@gmail.com>
2017-05-24 17:06:12 +0700

    check is text file

--- server/restafary.js
+++ server/restafary.js
@@ -8,6 +8,7 @@ const mellow = require('mellow');
 const ponse = require('ponse');
 const WIN = process.platform === 'win32';
 const CWD = process.cwd();
+const isTextPath = require('is-text-path');
 const Fs = {};
 
 [
@@ -27,43 +28,43 @@ function middle(options, request, response, next) {
     const isFile  = /^\/restafary\.js/.test(req.url);
     const prefix  = options.prefix || '/fs';
     const root    = options.root || '/';
-    
+
     const params  = {
         root,
         request,
         response,
     };
-    
+
     let name = ponse.getPathName(req);
     const is = !name.indexOf(prefix);
-    
+
     if (isFile)
         sendFile(req, res);
     else if (!is) {
         next();
     } else {
         name = name.replace(prefix, '') || '/';
-        
+
         params.name = name;
-        
+
         onFS(params, (error, options, data) => {
             params.gzip = !error;
-            
+
             if (options.name)
                 params.name = options.name;
-            
+
             if (options.gzip !== undefined)
                 params.gzip = options.gzip;
-            
+
             if (options.query)
                 params.query = options.query;
-            
+
             if (error)
                 return ponse.sendError(error, params);
-            
+
             if (!data)
                 data = getMsg(name, req);
-            
+
             ponse.send(data, params);
         });
     }
@@ -72,7 +73,7 @@ function middle(options, request, response, next) {
 function sendFile(request, response) {
     const url = request.url;
     const name = path.join(__dirname, '..', 'dist', url);
-    
+
     ponse.sendFile({
         name,
         gzip: true,
@@ -85,9 +86,9 @@ function getMsg(name, req) {
     let msg;
     const query = ponse.getQuery(req);
     const method = req.method.toLowerCase();
-    
+
     name = path.basename(name);
-    
+
     if (method !== 'put') {
         msg     = method;
     } else {
@@ -96,9 +97,9 @@ function getMsg(name, req) {
         else
             msg = 'save';
     }
-    
+
     msg     = format(msg, name);
-    
+
     return msg;
 }
 
@@ -117,26 +118,26 @@ function onFS(params, callback) {
     const p               = params;
     const name            = p.name;
     const query           = ponse.getQuery(p.request);
-    
+
     const optionsDefaults  = {
         gzip: false,
         name: '.txt'
     };
-    
+
     let root;
     if (typeof params.root === 'function')
         root = params.root();
     else
         root = params.root;
-    
+
     root = handleDotFolder(root);
     const rootWin = root.replace('/', '\\');
     const pathOS  = mellow.pathToWin(name, root);
     const pathWeb = path.join(root, name);
-    
+
     if (WIN && pathWeb.indexOf(rootWin) || !WIN && pathWeb.indexOf(root))
         return callback(Error('Path ' + pathWeb + ' beyond root ' + root + '!'), optionsDefaults);
-    
+
     switch (p.request.method) {
     case 'PUT':
         if (!checkPath(name, root))
@@ -146,13 +147,13 @@ function onFS(params, callback) {
                 callback(error, optionsDefaults);
             });
         break;
-    
+
     case 'PATCH':
         Fs.patch(path, p.request, (error) => {
             callback(error, optionsDefaults);
         });
         break;
-    
+
     case 'GET':
         Fs.get(query, pathOS, (error, data) => {
             onGet({
@@ -166,7 +167,7 @@ function onFS(params, callback) {
             }, callback);
         });
         break;
-    
+
     case 'DELETE':
         Fs.delete(query, pathOS, p.request, (error) => {
             callback(error, optionsDefaults);
@@ -179,67 +180,72 @@ function onGet(p, callback) {
     let options = {};
     const isFile  = p.error && p.error.code === 'ENOTDIR';
     const isStr   = typeof p.data === 'string';
-    
+
     const params  = {
         gzip: true,
         name: p.path,
         request: p.request,
         response: p.response,
     };
-    
+
     if (isFile)
         return fs.realpath(p.path, (error, path) => {
             if (!error)
                 params.name = path;
-            
+
             params.gzip = false;
             ponse.sendFile(params);
         });
-        
+
     if (p.error)
         return callback(p.error, options);
-    
+
     if (/^(size|time|hash|beautify|minify)$/.test(p.query))
         return callback(p.error, options, String(p.data));
-    
+
     p.data.path = addSlashToEnd(p.name);
-    
+
+    p.data.files.forEach(file => {
+        let path = p.data.path + file.name;
+        file.isText = isTextPath(path);
+    });
+
     if (p.name === '/')
         p.name += 'fs';
-    
+
     options     = {
         name    : p.name + 'fs.json',
         query   : p.query
     };
-    
+
     let str;
     if (isStr)
         str = p.data;
     else
         str = jonny.stringify(p.data, null, 4);
-    
+
     callback(p.error, options, str);
 }
 
 function format(msg, name) {
     const status = 'ok';
-    
+
     if (name)
         name = '("' + name + '")';
-    
+
     return msg + ': ' + status + name;
 }
 
 function addSlashToEnd(path) {
     if (!path)
         return path;
-    
+
     const length  = path.length - 1;
     const isSlash = path[length] === '/';
-    
+
     if (!isSlash)
         path += '/';
-    
+
     return path;
 }
 
